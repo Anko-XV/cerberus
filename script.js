89696574dc78c0c2c654f728f70ba0dd750dc817
Anko-XV <kwameobeng9@gmail.com>
2024-05-29 17:43:26 +0200

    29-05-2024

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const progress = document.querySelector("#progress")
 const feed = document.querySelector("#feed")
 const pet = document.querySelector("#pet")
 const hit = document.querySelector("#hit")
+// hulp van mees
+function imagechanger() {
+    document.getElementById("neutral").src = "./images/stage 1.5 cerberus.png";
+    score ++;
+    console.log(score);
+    updateScore();
+}
+
+function imagechangernegative() {
+    document.getElementById("neutral").src = "./images/stage 1.5 cerberus.png";
+    score --;
+    console.log(score);
+    updateScore();
+}
+
 // met hulp van milo //
 let score = 0;
 function updateScore(){
@@ -52,6 +67,10 @@ function imagechangernegative() {
     updateScore();
 }
 
+
+
 feed.addEventListener("click", imagechanger)
 pet.addEventListener("click", imagechanger)
 hit.addEventListener("click", imagechangernegative)
+
+honden = ["./images/stage 1.5 cerberus.png","./images/stage 2.5 cerberus.png","./images/stage 3.5 cerberus.png"]
